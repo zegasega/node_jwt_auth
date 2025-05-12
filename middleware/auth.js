48f60f2e2ca06328608b6c35ba7b79a80060ae41
zegasega <114316476+zegasega@users.noreply.github.com>
2025-05-12 11:37:43 +0300

    token expried time changed to 1 to 3h

--- middleware/auth.js
+++ middleware/auth.js
@@ -30,7 +30,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '1h' }
   );
 };
 
@@ -38,7 +38,7 @@ const generateRefreshToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email },
     process.env.REFRESH_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: '3h' }
   );
 };
 
