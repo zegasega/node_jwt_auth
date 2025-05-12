c919b8cc32c4306692194b36fb3adf10625b87fa
zegasega <114316476+zegasega@users.noreply.github.com>
2025-05-12 16:02:02 +0300

    WORKING ON AWS SES SERVCE

---
+++ services/emailService/config.js
@@ -0,0 +1,15 @@
+// config.js
+
+module.exports = {
+    aws: {
+        accessKeyId: 'YOUR_AWS_ACCESS_KEY',
+        secretAccessKey: 'YOUR_AWS_SECRET_ACCESS_KEY',
+        region: 'us-east-1',
+        sourceEmail: 'your-email@example.com', 
+    },
+
+    app: {
+        port: 3000,
+        baseUrl: 'http://localhost:3000',
+    }
+};
