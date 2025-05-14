71093a2e099a97810321e944f4b50bea040ed9b7
zegasega <114316476+zegasega@users.noreply.github.com>
2025-05-14 10:23:13 +0300

    force:true for reseting tables when we start the server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 db.sequelize.authenticate()
   .then(() => {
     console.log('Database connection successful');
-    return db.sequelize.sync({alter: true});
+    return db.sequelize.sync({alter: true, force: true});
   })
   .then(() => {
     console.log('Tables synchronized');
